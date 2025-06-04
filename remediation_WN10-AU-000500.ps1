ab0c94b9f689a1971f48fe98c8359bf3abb99f70
Ayub Ali <pltsummary@gmail.com>
2025-06-04 16:05:46 -0600

    Create remediation_WN10-AU-000500.ps1

---
+++ remediation_WN10-AU-000500.ps1
@@ -0,0 +1,24 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+
+.NOTES
+    Author          : Ayub Ali
+    GitHub          : https://github.com/ayubhali/stig-remediation-win10
+    Date Created    : 2025-06-04
+    Last Modified   : 2025-06-04
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-AU-000500
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Example usage:
+    PS C:\> .\remediation_WN10-AU-000500.ps1
+#>
