a64a99e6e2177051d571c46acd1a0e66c3d63473
Ayub Ali <pltsummary@gmail.com>
2025-06-05 13:31:40 -0600

    Create remediation_WN10-CC-000066.ps1

---
+++ remediation_WN10-CC-000066.ps1
@@ -0,0 +1,35 @@
+<#
+.SYNOPSIS
+    Enables command line data logging in process creation events in compliance with STIG ID WN10-CC-000066.
+
+.NOTES
+    Author          : Ayub Ali
+    GitHub          : https://github.com/ayubhali/stig-remediation-win10
+    Date Created    : 2025-06-05
+    Last Modified   : 2025-06-05
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000066
+
+.TESTED ON
+    Date(s) Tested  : 2025-06-05
+    Tested By       : Ayub Ali
+    Systems Tested  : Windows 10 Pro 22H2
+    PowerShell Ver. : 5.1+
+
+.USAGE
+    Example usage:
+    PS C:\> .\remediation_WN10-CC-000066.ps1
+#>
+
+$regPath = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System\Audit"
+$name = "ProcessCreationIncludeCmdLine_Enabled"
+$value = 1
+
+if (-not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
+}
+
+Set-ItemProperty -Path $regPath -Name $name -Value $value -Type DWord
+Write-Output "Command line data will now be included in process creation events."
