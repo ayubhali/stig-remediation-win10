fa7090651168cb949a34150fb7c3b7b0fefd95bb
Ayub Ali <pltsummary@gmail.com>
2025-06-05 13:37:03 -0600

    Create remediation_WN10-CC-000250.ps1

---
+++ remediation_WN10-CC-000250.ps1
@@ -0,0 +1,35 @@
+<#
+.SYNOPSIS
+    Enables SmartScreen filter for Microsoft Edge in compliance with STIG ID WN10-CC-000250.
+
+.NOTES
+    Author          : Ayub Ali
+    GitHub          : https://github.com/ayubhali/stig-remediation-win10
+    Date Created    : 2025-06-05
+    Last Modified   : 2025-06-05
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000250
+
+.TESTED ON
+    Date(s) Tested  : 2025-06-05
+    Tested By       : Ayub Ali
+    Systems Tested  : Windows 10 Pro 22H2
+    PowerShell Ver. : 5.1+
+
+.USAGE
+    Example usage:
+    PS C:\> .\remediation_WN10-CC-000250.ps1
+#>
+
+$regPath = "HKLM:\SOFTWARE\Policies\Microsoft\MicrosoftEdge\PhishingFilter"
+$name = "EnabledV9"
+$value = 1
+
+if (-not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
+}
+
+Set-ItemProperty -Path $regPath -Name $name -Value $value -Type DWord
+Write-Output "SmartScreen for Microsoft Edge has been enabled."
