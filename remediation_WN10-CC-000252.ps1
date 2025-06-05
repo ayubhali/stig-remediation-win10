686611733fe6c7c242c7ff7d4548f7ff8cf36d53
Ayub Ali <pltsummary@gmail.com>
2025-06-05 12:58:36 -0600

    Create remediation_WN10-CC-000252.ps1

---
+++ remediation_WN10-CC-000252.ps1
@@ -0,0 +1,35 @@
+<#
+.SYNOPSIS
+    Disables Windows Game Recording and Broadcasting in compliance with STIG ID WN10-CC-000252.
+
+.NOTES
+    Author          : Ayub Ali
+    GitHub          : https://github.com/ayubhali/stig-remediation-win10
+    Date Created    : 2025-06-05
+    Last Modified   : 2025-06-05
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000252
+
+.TESTED ON
+    Date(s) Tested  : 2025-06-05
+    Tested By       : Ayub Ali
+    Systems Tested  : Windows 10 Pro 22H2
+    PowerShell Ver. : 5.1+
+
+.USAGE
+    Example usage:
+    PS C:\> .\remediation_WN10-CC-000252.ps1
+#>
+
+$regPath = "HKLM:\Software\Policies\Microsoft\Windows\GameDVR"
+$name = "AllowGameDVR"
+$value = 0
+
+if (-not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
+}
+
+Set-ItemProperty -Path $regPath -Name $name -Value $value -Type DWord
+Write-Output "Windows Game Recording and Broadcasting has been disabled."
