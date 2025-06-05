80a4d062399e11cd48c7242ebcbfe7f4de770169
Ayub Ali <pltsummary@gmail.com>
2025-06-05 13:26:30 -0600

    Create emediation_WN10-CC-000180.ps1

---
+++ emediation_WN10-CC-000180.ps1
@@ -0,0 +1,35 @@
+<#
+.SYNOPSIS
+    Disables Autoplay for non-volume devices in compliance with STIG ID WN10-CC-000180.
+
+.NOTES
+    Author          : Ayub Ali
+    GitHub          : https://github.com/ayubhali/stig-remediation-win10
+    Date Created    : 2025-06-05
+    Last Modified   : 2025-06-05
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000180
+
+.TESTED ON
+    Date(s) Tested  : 2025-06-05
+    Tested By       : Ayub Ali
+    Systems Tested  : Windows 10 Pro 22H2
+    PowerShell Ver. : 5.1+
+
+.USAGE
+    Example usage:
+    PS C:\> .\remediation_WN10-CC-000180.ps1
+#>
+
+$regPath = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"
+$name = "NoAutoplayfornonVolume"
+$value = 1
+
+if (-not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
+}
+
+Set-ItemProperty -Path $regPath -Name $name -Value $value -Type DWord
+Write-Output "Autoplay has been turned off for non-volume devices."
