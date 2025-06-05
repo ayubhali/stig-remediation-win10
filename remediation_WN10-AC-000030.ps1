bc971896a32e32d281bf169d7756eb145a8733ea
Ayub Ali <pltsummary@gmail.com>
2025-06-05 13:32:55 -0600

    Create remediation_WN10-AC-000030.ps1

---
+++ remediation_WN10-AC-000030.ps1
@@ -0,0 +1,27 @@
+<#
+.SYNOPSIS
+    Sets the minimum password age to 1 day in compliance with STIG ID WN10-AC-000030.
+
+.NOTES
+    Author          : Ayub Ali
+    GitHub          : https://github.com/ayubhali/stig-remediation-win10
+    Date Created    : 2025-06-05
+    Last Modified   : 2025-06-05
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-AC-000030
+
+.TESTED ON
+    Date(s) Tested  : 2025-06-05
+    Tested By       : Ayub Ali
+    Systems Tested  : Windows 10 Pro 22H2
+    PowerShell Ver. : 5.1+
+
+.USAGE
+    Example usage:
+    PS C:\> .\remediation_WN10-AC-000030.ps1
+#>
+
+net accounts /minpwage:1
+Write-Output "Minimum password age has been set to 1 day."
